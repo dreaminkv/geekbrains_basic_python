96b08ddf98cb250d7c3113131101c33836627f96
Dream <inkver@gmail.com>
2021-07-06 15:56:29 +0300

    Add task

---
+++ dz_6/task_6_1.py
@@ -0,0 +1,16 @@
+# Не используя библиотеки для парсинга, распарсить (получить определённые данные)
+# файл логов web-сервера nginx_logs.txt
+# (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs)
+# — получить список кортежей вида: (<remote_addr>, <request_type>, <requested_resource>). Например:
+# [
+#     ...
+#     ('141.138.90.60', 'GET', '/downloads/product_2'),
+#     ('141.138.90.60', 'GET', '/downloads/product_2'),
+#     ('173.255.199.22', 'GET', '/downloads/product_2'),
+#     ...
+# ]
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    lines = [line.replace('"', '').split() for line in f.readlines()]
+    for line in lines:
+        print((line[0], line[5], line[6]))
+
